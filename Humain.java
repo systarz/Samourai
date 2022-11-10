7b0472daa8fdc2c790308fb93a52fc4a979d9d67
sylvainboyer <sylvainboyer@172.17.143.179>
2022-11-10 15:04:15 +0100

    premiere partie

--- Humain.java
+++ Humain.java
@@ -1,17 +1,37 @@
 
 public class Humain {
+	private nom;
+	private argent;
+	private boisson;
+	
 	public Humain(String nom, int argent, String boisson) {
 		this.nom = nom;
 		this.argent = argent;
 		this.boisson = boisson;
 	}
+	
 	public void parler(String texte){
-		this.nom + " - " + texte;
+		System.out.println(this.nom + " - " + texte);
 	}
+	
 	public void direBonjour(){
 		this.parler("Bonjour ! " + "Je m'appelle " + this.nom + " et j'aime boire du " + this.boisson + ".");
 	}
 	public void boire() {
 		this.parler("Mmm, un bon verre de " + this.boisson + " ! GLOUPS !");
 	}
+	
+	private getArgent() {
+		return this.argent;
+
+	}
+	
+	private getBoisson() {
+		return this.boisson;
+
+	}
+	private getNom() {
+		return this.nom;
+	}
+	
 }
