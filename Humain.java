067d6523b4c44ba1b6773c71cffc578af87470ca
sylvainboyer <sylvainboyer@172.17.143.179>
2022-11-10 14:40:30 +0100

    v3

--- Humain.java
+++ Humain.java
@@ -1,4 +1,17 @@
 
 public class Humain {
-
+	public Humain(String nom, int argent, String boisson) {
+		this.nom = nom;
+		this.argent = argent;
+		this.boisson = boisson;
+	}
+	public void parler(String texte){
+		this.nom + " - " + texte;
+	}
+	public void direBonjour(){
+		this.parler("Bonjour ! " + "Je m'appelle " + this.nom + " et j'aime boire du " + this.boisson + ".");
+	}
+	public void boire() {
+		this.parler("Mmm, un bon verre de " + this.boisson + " ! GLOUPS !");
+	}
 }
